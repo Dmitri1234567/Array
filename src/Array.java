8b3a4e031af888cfbe160f85099a12e4fb71d04d
Dima <dmitrijskurcenko3@gmail.com>
2022-10-19 11:37:38 +0300

    Array

---
+++ src/Array.java
@@ -0,0 +1,11 @@
+public class Array {
+    public static void main(String[] args) {
+        int month[];
+        month=new int[12];
+        for(int i=0;i<month.length;i++)
+        {
+            month[i]=i;
+            System.out.println(month[i]);
+        }
+    }
+}
